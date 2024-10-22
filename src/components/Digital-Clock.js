0a7533658d8bb5cb816afb20f696d3a7757924a4
Neeraj Verma <neerajverma30699@gmail.com>
2024-10-22 23:42:09 +0530

    Initial commit

---
+++ src/components/Digital-Clock.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const Clock = ()=>{
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setTime(new Date());
+        },1000);
+        return ()=>{
+            clearInterval(interval)}
+    },[]);
+
+    const formatTime = () =>{
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        const meridiem = hours >= 12 ? "PM" : "AM";
+        
+        hours = hours % 12 || 12;
+        
+        return `${hours}:${minutes}:${seconds} ${meridiem}`;
+    }
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-fuchsia-400">
+            <span className="text-9xl text-gray-700 font-bold">{formatTime()}</span>
+        </div>
+    )
+}
+
+export default Clock;
